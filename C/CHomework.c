12915ae280350dd91902927556dcd3894af58ff8
alexandervansteel <alexandervansteel@gmail.com>
2016-09-25 00:59:29 -0400

    Update to homework

--- C/CHomework.c
+++ C/CHomework.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int FillTable(int a[], int max){
-  for(int i=0;i<=max;i++){
-    a[i] = i;
+int FillTable(int a[], int* max){
+  int i, input;
+  char c; 
+  for(i=0;i<=*max;i++){
+    printf("Enter an integer or' Q' or 'q' to quit: ");
+    if(scanf("%d", &input) == 1){
+      a[i]= input;
+    }
+    else{
+      (scanf("%c",&c) == 1);
+      if((c=='Q') || (c=='q')){
+        *max = i - 1;
+        break;
+      } else{
+        printf("Invalid input. Please try again.\n");
+        i--;
+      }
+    }
   }
+  printf("Table complete.\n");
 }
 
 void PrintReverseTable(int a[], int max){
@@ -13,9 +31,17 @@ void PrintReverseTable(int a[], int max){
   }
 }
 
-int int main(int argc, char const *argv[]) {
-  int table[100];
-  FillTable(table,10);
-  PrintReverseTable(table,10);
+int main(int argc, char *argv[]){
+  int max;
+
+  printf("Enter the maximum number of elements: ");
+  scanf("%d", &max);
+  max--;
+
+  int table[max];
+  memset(table, 0, sizeof(table));
+  FillTable(table,&max);
+  
+  PrintReverseTable(table,max);
   return 0;
 }
