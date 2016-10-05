ac98b7c2f2705024f74200cc05dc6fa8243987d2
alexandervansteel <alexandervansteel@gmail.com>
2016-10-05 14:20:31 -0400

    added write file

--- C/BMSErrorCheck.c
+++ C/BMSErrorCheck.c
@@ -14,36 +14,47 @@ int operand(char *line, int *error_cnt);
 */
 
 /*
- * Auther: Alexander Vansteel
- * Assignment: C Homework 2
- * Due Date: 2016 October 10
- */
- int main(int argc, char *argv[]){
-    char file_name[100];
+* Auther: Alexander Vansteel
+* Assignment: C Homework 2
+* Due Date: 2016 October 10
+*/
+int main(int argc, char *argv[]){
+  char file_name[100];
 
-    memset(file_name,0,sizeof(file_name));
-    printf("Enter the name of the file to scan: ");
-    //fgets(file_name,sizeof(file_name),stdin);
-	scanf("%s", file_name);
+  memset(file_name,0,sizeof(file_name));
+  printf("Enter the name of the file to scan: ");
+  //fgets(file_name,sizeof(file_name),stdin);
+  scanf("%s", file_name);
 
-	printf("Requested file name: %s\n", file_name);
-    check_file(file_name);
-    return 0;
- }
+  printf("Requested file name: %s\n", file_name);
+  check_file(file_name);
+  return 0;
+}
 
- int check_file(char *file_name){
-    FILE *fp;
-	char line[500];
+int check_file(char *file_name){
+  FILE *fp;
+  char line[500];
 
-    if((fp = fopen(file_name, "r")) == NULL){
-        perror("File name error");
-        return 1;
-    }
-    
-	printf("Opening requested file: %s\n", file_name);
-	while(fgets(line,100,fp) != NULL){
-		printf("%s",line);
-	}
+  if((fp = fopen(file_name, "r")) == NULL){
+    perror("File name error");
+    return 1;
+  }
 
-    return 0;
- }
+  printf("Opening requested file: %s\n", file_name);
+
+  /* Creates new file to store the corrections in. */
+  char c_file[5000] = "correction_";
+  strcat(c_file, file_name);
+  printf("Error Correction File: %s\n",c_file);
+  FILE *cfp;
+  if((cfp = fopen(c_file, "a")) == NULL){
+    perror("Error creating correction file.");
+    return 1;
+  }
+
+  while(fgets(line,100,fp) != NULL){
+    printf("%s",line);
+  }
+
+  return 0;
+}
