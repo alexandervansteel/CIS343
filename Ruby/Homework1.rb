e17644d19efb29dd0d11ad1d2dbbf98e44400039
alexandervansteel <alexandervansteel@gmail.com>
2016-10-22 21:44:41 +0000

    first ruby homework

---
+++ Ruby/Homework1.rb
@@ -0,0 +1,18 @@
+# Homework1
+# Alexander Vansteel
+# 2016 October 26
+
+class String
+  def hex_to_dec
+    self.each_char{|char|
+      if self[char] =~ /\H/
+        return puts "Error: #{self[char]} is not a valid Hex digit."
+      end
+    }
+    return puts "The Hex number #{self} is #{self.to_i(16)} in Decimal."
+  end
+end
+
+print "Input a Hex number to convert to Decimal: "
+u_input = gets.upcase.gsub(/\s+/,"").chomp
+u_input.hex_to_dec
